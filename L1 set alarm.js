d4157b0c7c7151a7f459d55669094112955b0fc6
bethanyspaniel <130386287+bethanyspaniel@users.noreply.github.com>
2023-09-03 17:53:25 +0000

    Added new codewars solution 9/3/23

---
+++ L1 set alarm.js
@@ -0,0 +1,26 @@
+// L1 Set Alarm
+// Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+// employed | vacation 
+// true     | true     => false
+// true     | false    => true
+// false    | true     => false
+// false    | false    => false
+
+// My solution
+function setAlarm(employed, vacation) {
+  if (employed == false) return false
+  else if (employed == true && vacation == false) return true
+  else return false
+}
+
+// Best solutions
+const setAlarm = (employed, vacation) => employed && !vacation;
+
+function setAlarm(employed, vacation) {
+  return employed && !vacation;
+}
+
+function setAlarm(employed, vacation) {
+  return (employed && !vacation) ? true : false;
+}
