40e51619c60a14ba84e72e68fa986219ac9ea5e4
bethanyspaniel <130386287+bethanyspaniel@users.noreply.github.com>
2023-09-06 14:02:10 +0000

    Added new codewars solution 9/6/23

---
+++ get ascii value of character.js
@@ -0,0 +1,14 @@
+// Get ASCII value of character
+// Get ASCII value of a character. For the ASCII table you can refer to http://www.asciitable.com/
+
+// My solution
+function getASCII(c) {
+  return c.charCodeAt(0)
+}
+
+// Best solution
+const getASCII = c => c.charCodeAt(0);
+
+function getASCII(c) {
+  return c.charCodeAt(0)
+}
