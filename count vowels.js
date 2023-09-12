18b16d5232b21e39e5ece1be38236faa0628a533
bethanyspaniel <130386287+bethanyspaniel@users.noreply.github.com>
2023-09-12 15:29:23 +0000

    Added codewars solutions

--- count vowels.js
+++ count vowels.js
@@ -8,3 +8,28 @@ function getCount(str) {
 }
 
 // Best solutions
+function getCount(str) {
+  return (str.match(/[aeiou]/ig) || []).length;
+}
+
+function getCount(str) {
+  var vowelsCount = 0;
+  var vowels = ["a", "e", "i", "o", "u"];
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < vowels.length; j++) {
+      if (str[i] === vowels[j]) {
+        vowelsCount++;
+      }
+    }
+  }
+
+  return vowelsCount;
+}
+
+function getCount(str) {
+  return str.split('').filter(c => "aeiouAEIOU".includes(c)).length;
+}
+
+function getCount(str) {
+  return str.replace(/[^aeiou]/gi, '').length;
+}
