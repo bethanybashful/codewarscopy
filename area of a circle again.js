cb67c7b46f52e00fcddefd144ad0afa341a36a3a
bethanyspaniel <130386287+bethanyspaniel@users.noreply.github.com>
2023-09-13 15:30:37 +0000

    Added new codewars solution 9/13/23

---
+++ area of a circle again.js
@@ -0,0 +1,9 @@
+// Area of a circle
+// Complete the function which will return the area of a circle with the given radius. Returned value is expected to be accurate up to tolerance of 0.01. If the radius is not positive, throw Error.
+
+// My solution
+function circleArea(radius) {
+  return radius > 0 ? Math.PI * (radius ** 2) : error;
+}
+
+// These solutions aren't the top ones, idk what codewars is doing but they're not showing me the best solutions at the top and I cannot find them
