54d1ef6b85900ed065df9c80822035c77dcd3e06
bethanyspaniel <130386287+bethanyspaniel@users.noreply.github.com>
2023-09-17 19:43:45 +0000

    Added new codewars solution 9/17/23

---
+++ drink about.js
@@ -0,0 +1,31 @@
+// Drink about
+// Kids drink toddy. Teens drink coke. Young adults drink beer. Adults drink whisky. Make a function that receive age, and return what they drink. Rules: Children under 14 old. Teens under 18 old. Young under 21 old. Adults have 21 or more.
+
+// My solution
+function peopleWithAgeDrink(age) {
+  if (age < 14) return 'drink toddy'
+  else if (age < 18) return 'drink coke'
+  else if (age < 21) return 'drink beer'
+  else return 'drink whisky'
+}
+
+// Best solution
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+    age < 18 ? "drink coke" :
+      age < 21 ? "drink beer" : "drink whisky"
+
+function peopleWithAgeDrink(age) {
+  if (age < 14)
+    drink = 'toddy';
+  else if (age < 18)
+    drink = 'coke'
+  else if (age < 21)
+    drink = 'beer';
+  else if (age => 21)
+    drink = 'whisky';
+
+  return 'drink ' + drink;
+}
+
+const peopleWithAgeDrink = old => 'drink ' + (old < 14 ? 'toddy' : old < 18 ? 'coke' : old < 21 ? 'beer' : 'whisky')
