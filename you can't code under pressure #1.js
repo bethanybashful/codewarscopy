9611bcf9f7e13dace303615b972bc8e36d487630
bethanybashful <130386287+bethanybashful@users.noreply.github.com>
2023-09-19 19:48:20 +0000

    Added new codewars solution 9/19/23

---
+++ you can't code under pressure #1.js
@@ -0,0 +1,10 @@
+// You can't code under pressure #1
+// Code as fast as you can! You need to double the integer and return it.
+
+// My solution
+function doubleInteger(i) {
+  return i * 2;
+}
+
+// Best solution
+const doubleInteger = i => 2 * i;
