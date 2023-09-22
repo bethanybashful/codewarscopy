b41896cdc059cdfafa806e9e03199dab02f78e28
bethanybashful <130386287+bethanybashful@users.noreply.github.com>
2023-09-22 15:19:00 +0000

    Added new codewars solution 9/22/23

---
+++ even or odd again 4.js
@@ -0,0 +1,5 @@
+// Even or odd
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// My solution
+const evenOrOdd = (number) => number % 2 == 0 ? "Even" : "Odd"
