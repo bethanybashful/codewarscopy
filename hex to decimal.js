806f89bc26d8ffabd0d40172634e429b664f36c8
bethanybashful <130386287+bethanybashful@users.noreply.github.com>
2023-10-05 15:09:06 +0000

    Added new codewars solution 10/5/23

---
+++ hex to decimal.js
@@ -0,0 +1,5 @@
+// Hex to Decimal
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// My solution, same as best
+const hexToDec = hexString => parseInt(hexString, 16)
