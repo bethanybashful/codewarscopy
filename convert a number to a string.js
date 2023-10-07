9855708d889f527d09b3650f68dd4dd962173a91
bethanybashful <130386287+bethanybashful@users.noreply.github.com>
2023-10-07 19:12:04 +0000

    Added new codewars solution 10/7/23

---
+++ convert a number to a string.js
@@ -0,0 +1,11 @@
+// Convert number to a string
+// We need a function that can transform a number (integer) into a string.
+
+// My solution same as best
+const numberToString = num => num.toString()
+
+// Best solution
+function numberToString(num) {
+  // Return a string of the number here!
+  return String(num);
+}
