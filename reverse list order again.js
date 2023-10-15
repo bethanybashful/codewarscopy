10a3572a4af279e8f91647618cb382b7b9edd483
bethanybashful <130386287+bethanybashful@users.noreply.github.com>
2023-10-15 15:08:30 +0000

    Added new codewars solution 10/15/23

---
+++ reverse list order again.js
@@ -0,0 +1,5 @@
+// Reverse list order
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// My solution same as top
+const reverseList = list => list.reverse()
