e59c864b59461e9a0a48c017ff7d91b1d370abbb
bethanybashful <130386287+bethanybashful@users.noreply.github.com>
2023-10-22 14:53:28 +0000

    Added new codewars solution 10/22/23

---
+++ simple multiplication again 2.js
@@ -0,0 +1,10 @@
+// Simple multiplication
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// My solution
+const simpleMultiplication = number => number % 2 ? number * 9 : number * 8;
+
+// Best solution
+function simpleMultiplication(n) {
+  return n * (n % 2 ? 9 : 8);
+}
