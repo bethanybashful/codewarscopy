4dc62262337a171e6ac0b39508d51b8b8b0173a9
bethanybashful <130386287+bethanybashful@users.noreply.github.com>
2023-10-29 17:50:28 +0000

    Added new codewars attempt 10/29/23

---
+++ leap years.js
@@ -0,0 +1,18 @@
+// Leap years
+// In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are: Years divisible by 4 are leap years, but years divisible by 100 are not leap years, but years divisible by 400 are leap years.
+
+// My attempt
+const isLeapYear = year => (year % 4 == 0 && year % 400 == 0)
+
+// function isLeapYear(year) {
+//   return (year % 4 && year % 400)
+// }
+
+// Solutions
+function isLeapYear(year) {
+  return (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
+}
+
+function isLeapYear(year) {
+  return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
